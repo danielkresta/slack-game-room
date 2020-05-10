5b25fbde5547886167436875d93a1e411631bdbf
Daniel Kresta <danielkresta3@gmail.com>
2020-05-10 21:19:28 +0200

    revert timeout config

--- src/configs.ts
+++ src/configs.ts
@@ -2,9 +2,9 @@ export const ALLOWED_CHANNELS = [
     "G013AEBN8FL",  // gr-test
 ];
 
-export const GARBAGE_COLLECTION_INTERVAL_MS = 1000 * 60 * 30;
+export const GARBAGE_COLLECTION_INTERVAL_MS = 1000 * 60 * 60;
 
-export const DEFAULT_GAME_TIMEOUT_MS = 1000 * 60 * 40;
+export const DEFAULT_GAME_TIMEOUT_MS = 1000 * 60 * 30;
 
 export const SLACK_COMMANDS = {
     football: "/helloworld",
